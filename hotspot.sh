e1aa1ddd8496afbd27cc9ab3ada8e0e5fe0a724d
Ajaybalajiprasad <ajaybalajiprasad02@gmail.com>
2026-08-08 10:59:33 +0530

    fix: 5ghz and 2.4ghz channel calculation

--- hotspot.sh
+++ hotspot.sh
@@ -48,7 +48,15 @@ fi
 FREQ=$(iw dev "$WIFI_IFACE" link | awk '/freq:/{print $2}')
 
 if [ -n "$FREQ" ]; then
-    CHANNEL=$(python3 -c "freq=int(float('$FREQ')); print(int((freq - 2407)/5))" 2>/dev/null || true)
+    CHANNEL=$(python3 -c "
+freq = int(float('$FREQ'))
+if 2412 <= freq <= 2484:
+    print(int((freq - 2407) / 5))
+elif 5170 <= freq <= 5825:
+    print(int((freq - 5000) / 5))
+else
+    print('')
+" 2>/dev/null || true)
 fi
 
 CHANNEL=${CHANNEL:-6}
